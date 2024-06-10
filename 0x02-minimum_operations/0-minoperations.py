424b207585c070b9a3fa1103b95caf1f5b7d9c7e
ZaneleNe <zanelenema97@gmail.com>
2024-06-10 10:53:09 +0000

    0-minoperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''Method that calculates the fewest number of operations needed to result
+in exactly n H characters in the file.
+'''
+
+
+def minOperations(n):
+    '''Calculates the fewest number of operations needed to result
+    in exactly n H characters.
+    '''
+def minOperations(n):
+    if n == 1:
+        return 0
+    ops = 0
+    i = 2
+    while i <= n:
+        if n % i == 0:
+            ops += i
+            n //= i
+        else:
+            i += 1
+    return ops
